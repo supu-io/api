41c27f7e20c75518f3422c4f6e677d90e99f24ff
Adria Cidre <adria.cidre@gmail.com>
2015-10-16 11:44:22 +0200

    Get statuses cleanup

--- get_statuses.go
+++ get_statuses.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
-	"time"
 
 	"github.com/go-martini/martini"
 )
@@ -13,26 +11,22 @@ import (
 func GetStatuses(r *http.Request, params martini.Params) string {
 	statuses, err := getStatuses()
 	if err != nil {
-		return "{\"error\":\"Internal error\"}"
+		return GenerateErrorMessage(err)
 	}
-	json, err := json.Marshal(statuses)
+	res, err := json.Marshal(statuses)
 	if err != nil {
-		log.Println(err)
-		return "{\"error\":\"Internal error\"}"
+		return GenerateErrorMessage(err)
 	}
 
-	return string(json)
+	return string(res)
 }
 
+// Get a list of valid statuses
 func getStatuses() ([]string, error) {
-	body := "{\"issue\":{\"id\":\"\",\"status\":\"\"}}"
-	res, err := nc.Request("workflow.states.all", []byte(body), 10000*time.Millisecond)
-	if err != nil {
-		return []string{}, err
-	}
+	res := Request("workflow.states.all", "")
 
 	statuses := []string{}
-	err = json.Unmarshal(res.Data, &statuses)
+	err := json.Unmarshal([]byte(res), &statuses)
 	if err != nil {
 		return []string{}, err
 	}
@@ -42,15 +36,13 @@ func getStatuses() ([]string, error) {
 
 // Checks if the given status is valid or not
 func isValidStatus(status string) (bool, []string) {
-	statuses, err := getStatuses()
-	if err != nil {
-		return false, statuses
-	}
-	sw := false
+	statuses, _ := getStatuses()
+
 	for _, s := range statuses {
 		if string(s) == status {
-			sw = true
+			return true, statuses
 		}
 	}
-	return sw, statuses
+
+	return false, statuses
 }

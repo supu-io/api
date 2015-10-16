3314043096c6a1d72f4484e8a98e6d0f039b3562
Adria Cidre <adria.cidre@gmail.com>
2015-10-16 15:40:44 +0200

    Use messages library on issues.create

--- create_issue.go
+++ create_issue.go
@@ -2,26 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
-	"time"
 
 	"github.com/go-martini/martini"
+	"github.com/supu-io/messages"
 )
-
-// CreateIssueType ...
-type CreateIssueType struct {
-	Title string `json:"title"`
-	Body  string `json:"body"`
-	Org   string `json:"owner"`
-	Repo  string `json:"repo"`
-}
-
-// CreateIssueMsg ...
-type CreateIssueMsg struct {
-	Issue   CreateIssueType `json:"issue"`
-	*Config `json:"config"`
-}
 
 // CreateAttr json to create an issue
 type CreateAttr struct {
@@ -31,39 +16,25 @@ type CreateAttr struct {
 	Repo  string `json:"repo"`
 }
 
-func (i *CreateIssueMsg) toJSON() []byte {
-	json, err := json.Marshal(i)
-	if err != nil {
-		log.Println(err)
-	}
-	return json
-
-}
-
 // CreateIssue is the POST /issue/:issue and updates an Issue status
 func CreateIssue(r *http.Request, params martini.Params) string {
 	var t CreateAttr
-
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&t)
+
 	if err != nil {
-		return "{\"error\":\"" + err.Error() + "\"}"
+		return GenerateErrorMessage(err)
 	}
 
-	msg := CreateIssueMsg{
-		Issue: CreateIssueType{
+	msg := messages.CreateIssue{
+		Issue: &messages.Issue{
 			Title: t.Title,
 			Body:  t.Body,
 			Repo:  t.Repo,
 			Org:   t.Org,
 		},
-		Config: getConfig(),
+		Config: config(),
 	}
 
-	issue, err := nc.Request("issues.create", msg.toJSON(), 10000*time.Millisecond)
-	if err != nil {
-		return "{\"error\":\"" + err.Error() + "\"}"
-	}
-
-	return string(issue.Data)
+	return Request("issues.create", msg)
 }

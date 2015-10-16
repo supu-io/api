0221dad7a5f0dbc9f00eee0d8babd73f9f2852a9
Adria Cidre <adria.cidre@gmail.com>
2015-10-16 15:50:11 +0200

    Setup to use shared library messages

--- setup.go
+++ setup.go
@@ -1,44 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/go-martini/martini"
+	"github.com/supu-io/messages"
 )
 
 // Setup the necessary tools to start using supu
 func Setup(r *http.Request, params martini.Params) string {
-
-	type SetupMsg struct {
-		Org     string   `json:"org"`
-		Repo    string   `json:"repo"`
-		States  []string `json:"states"`
-		*Config `json:"config"`
-	}
 
 	states, err := getStatuses()
 	if err != nil {
 		return "{\"error\":\"" + err.Error() + "\"}"
 	}
 
-	msg := SetupMsg{
+	msg := messages.Setup{
 		Org:    r.URL.Query().Get("org"),
 		Repo:   r.URL.Query().Get("repo"),
 		States: states,
-		Config: getConfig(),
+		Config: config(),
 	}
 
-	body, err := json.Marshal(msg)
-	if err != nil {
-		return "{\"error\":\"" + err.Error() + "\"}"
-	}
-
-	issues, err := nc.Request("issue-tracker.setup", body, 10000*time.Millisecond)
-	if err != nil {
-		return "{\"error\":\"" + err.Error() + "\"}"
-	}
-
-	return string(issues.Data)
+	return Request("issue-tracker.setup", msg)
 }
--- types.go
+++ types.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-)
-
 // Github configuration structure
 type Github struct {
 	Token string `json:"token"`
@@ -13,23 +8,6 @@ type Github struct {
 // Config structure
 type Config struct {
 	Github `json:"github"`
-}
-
-// IssuesList list of issues
-type IssuesList struct {
-	Status  string `json:"status"`
-	Org     string `json:"org, omitempty"`
-	Repo    string `json:"repo, omitempty"`
-	*Config `json:"config"`
-}
-
-// Convert issues list to a json string
-func (i *IssuesList) toJSON() []byte {
-	json, err := json.Marshal(i)
-	if err != nil {
-		log.Println(err)
-	}
-	return json
 }
 
 // UpdateAttr json to update an issue
